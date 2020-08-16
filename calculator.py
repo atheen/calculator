0d833f2e530ed860b2c2dd5a50c7e7585c726ba2
atheen aleesa <atheen@atheens-iMac.local>
2020-08-16 13:27:52 +0300

    completed the calculator

--- calculator.py
+++ calculator.py
@@ -1,8 +1,27 @@
 
 def main():
 	#write your code here
+	first_num = input("Enter the first number: ")
+	second_num = input("Enter the second number: ")
+	operation = input("Enter the operation(+, -, /, *): ")
+	if first_num.isnumeric() == False or second_num.isnumeric() == False:
+		print("invalid numbers")
+	elif operation != "+" and operation != "-" and operation != "*" and operation != "/":
+		print("invalid operation")
+	else:
+		first_num = int(first_num)
+		second_num = int(second_num)
+		if operation == "+":
+			result = first_num + second_num
+		elif operation == "-":
+			result = first_num - second_num
+		elif operation == "/":
+			result = first_num / second_num
+		elif operation == "*":
+			result = first_num * second_num
+	print("The answer is %s"%(result))
 	pass
-	
+
 
 
 
